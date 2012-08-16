d4d5004785f955d4ef0ebc4e7e2fb3fb7ac3cfce
Olivier Mehani <olivier.mehani@nicta.com.au>
2012-08-16 13:52:55 +1000

    Update autoclean.sh
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- autoclean.sh
+++ autoclean.sh
@@ -1,13 +1,8 @@
-#!/bin/sh
-
-RM=`which rm`
-
-set -x
+#!/bin/sh -x
 
 test -f Makefile && make distclean
 
-test -d autom4te.cache && ${RM} -fr autom4te.cache
+test -d autom4te.cache && rm -fr autom4te.cache
 
-find . -name Makefile.in -exec ${RM} -f {} +
-${RM} -f aclocal.m4 config.h.in config.guess config.sub configure depcomp install-sh ltmain.sh missing m4/*.m4
-
+find . -name Makefile.in -exec rm -f {} +
+rm -rf aclocal.m4 build-aux/ config.h.in configure m4/00gnulib.m4 m4/gnulib-com* m4/gnulib-tool.m4 m4/lt*.m4
