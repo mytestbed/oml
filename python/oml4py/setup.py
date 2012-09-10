c0ab43670f99bbdb4b41d214e5c532a129627266
Fraida Fund <ffund01@students.poly.edu>
2012-09-10 20:20:00 +1000

    Update package name and license.
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- python/oml4py/setup.py
+++ python/oml4py/setup.py
@@ -1,9 +1,17 @@
 from distutils.core import setup
-setup(name='oml',
-      version='1.0',
+setup(name='oml4py',
+      version='1.0.0',
       author = "Fraida Fund",
       author_email = "ffund01@students.poly.edu",
       description = ("An OML client module for Python"),
       url = "http://witestlab.poly.edu",
-      py_modules=['oml'],
+      download_url = "http://pypi.python.org/pypi/oml4py",
+      py_modules=['oml4py'],
+      license = "MIT",
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Topic :: Scientific/Engineering',
+      ],
+      long_description=open('README.txt', 'rt').read(),
       )
