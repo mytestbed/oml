1022ad5361a9eae572a4658e8240e10fbbec0827
Olivier Mehani <olivier.mehani@nicta.com.au>
2012-11-30 11:51:13 +1100

    Update .gitignore and auto{gen,clean}.sh
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- autoclean.sh
+++ autoclean.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -x
 
-test -f Makefile && make distclean
-
-test -d autom4te.cache && rm -fr autom4te.cache
+test -f Makefile && make maintainer-clean
 
 find . -name Makefile.in -exec rm -f {} +
-rm -rf aclocal.m4 build-aux/ config.h.in configure m4/00gnulib.m4 m4/gnulib-com* m4/gnulib-tool.m4 m4/lt*.m4
+find build-aux/ -type f -not -name gen-authors.sh -exec rm {} +
+rm -rf aclocal.m4 config.h.in configure gnulib/ m4/00gnulib.m4 m4/gnulib-com* m4/gnulib-tool.m4 m4/lt*.m4 m4/libtool.m4
--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 rm -rf autom4te.cache/
-gnulib-tool --update --quiet --quiet
+# Let the user know what we do, tersely
+echo "gnulib-tool: updating portability libary files" >&2
+gnulib-tool --update --quiet --quiet >/dev/null
 chmod a+x build-aux/git-version-gen
 autoreconf -i $*
