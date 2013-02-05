4202ca37ce235a03b848efbf35c06efd81096d1e
Olivier Mehani <olivier.mehani@nicta.com.au>
2013-02-05 17:24:32 +1100

    Make CHECK test verbosity configurable, and verbose by default
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- test/lib/check_libshared.c
+++ test/lib/check_libshared.c
@@ -36,7 +36,7 @@ main (void)
   SRunner *sr = srunner_create (util_suite ());
   srunner_add_suite (sr, headers_suite ());
 
-  srunner_run_all (sr, CK_NORMAL);
+  srunner_run_all (sr, CK_ENV);
   number_failed += srunner_ntests_failed (sr);
   srunner_free (sr);
   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
--- test/server/check_server.c
+++ test/server/check_server.c
@@ -37,7 +37,7 @@ main (void)
   srunner_add_suite (sr, binary_protocol_suite ());
   //  srunner_add_suite (sr, database_suite ()); /* For example ... */
 
-  srunner_run_all (sr, CK_NORMAL);
+  srunner_run_all (sr, CK_ENV);
   number_failed += srunner_ntests_failed (sr);
   srunner_free (sr);
   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
