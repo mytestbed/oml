ac1f038c2c52cfd6f8f1c8e3af81da5cd9c6818c
Olivier Mehani <shtrom@ssji.net>
2013-10-11 13:20:15 +1100

    Use Git's .mailmap feature to simplify author list generation
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

--- build-aux/gen-authors.sh
+++ build-aux/gen-authors.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 
-echo '# Copyright (c) 2007-2012 Nicta, OML Contributors <oml-user@lists.nicta.com.au>'
+echo '# Copyright (c) 2007-2013 Nicta, OML Contributors <oml-user@lists.nicta.com.au>'
 echo '#'
 echo '# Licensing information can be found in file COPYING.'
 echo ''
@@ -12,8 +12,7 @@ echo ''
   echo 'Manpreet Singh'
   echo 'Ivan Seskar'
   echo 'Pandurang Kama'
-  git log | sed -n -e 's/^Author: *//p'
+  git log --format="%aN"
 ) \
-  | sed -e 's/ *<.*>.*//' \
   | sort \
   | uniq
