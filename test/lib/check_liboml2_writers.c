388ebc957444609dd2fd448c81a24462e67c25b7
Olivier Mehani <olivier.mehani@nicta.com.au>
2014-09-30 18:26:34 +1000

    Test Zlib compression
    refs: #1798
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- test/lib/check_liboml2_writers.c
+++ test/lib/check_liboml2_writers.c
@@ -117,16 +117,159 @@ END_TEST
 
 #define ZFN	"test_zw_create_buffered"
 
+/* def() and inf() adapted from the (public domain) zlib usage example at [0]
+ * to use (de/in)flateInit2 with -15 as the windowBits to not include any
+ * header/trailer.
+ * [0] http://zlib.net/zlib_how.html
+ */
+#define CHUNK 512
+
+/* Compress from file source to file dest until EOF on source.
+   def() returns Z_OK on success, Z_MEM_ERROR if memory could not be
+   allocated for processing, Z_STREAM_ERROR if an invalid compression
+   level is supplied, Z_VERSION_ERROR if the version of zlib.h and the
+   version of the library linked do not match, or Z_ERRNO if there is
+   an error reading or writing the files. */
+int def(FILE *source, FILE *dest, int level)
+{
+    int ret, flush;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+
+    /* allocate deflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    ret = deflateInit2(&strm, level, Z_DEFLATED, -15, 8, Z_DEFAULT_STRATEGY);
+    if (ret != Z_OK) {
+      return ret;
+    }
+
+    /* compress until end of file */
+    do {
+
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)deflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
+        strm.next_in = in;
+
+        /* run deflate() on input until output buffer not full, finish
+           compression if all of source has been read in */
+        do {
+
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+
+            ret = deflate(&strm, flush);    /* no bad return value */
+            fail_unless(ret != Z_STREAM_ERROR, "Zlib deflate state clobbered");  /* state not clobbered */
+
+           have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)deflateEnd(&strm);
+                return Z_ERRNO;
+            }
+
+        } while (strm.avail_out == 0);
+        fail_unless(strm.avail_in == 0, "Not all input used by the end of def()");     /* all input will be used */
+
+        /* done when last data in file processed */
+    } while (flush != Z_FINISH);
+    fail_unless(ret == Z_STREAM_END, "Zlib deflate stream not finished");        /* stream will be complete */
+
+    /* clean up and return */
+    (void)deflateEnd(&strm);
+    return Z_OK;
+}
+
+/* Decompress from file source to file dest until stream ends or EOF.
+   inf() returns Z_OK on success, Z_MEM_ERROR if memory could not be
+   allocated for processing, Z_DATA_ERROR if the deflate data is
+   invalid or incomplete, Z_VERSION_ERROR if the version of zlib.h and
+   the version of the library linked do not match, or Z_ERRNO if there
+   is an error reading or writing the files. */
+int inf(FILE *source, FILE *dest)
+{
+    int ret;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+
+    /* allocate inflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in = Z_NULL;
+    ret = inflateInit2(&strm, -15);
+    if (ret != Z_OK)
+        return ret;
+
+    /* decompress until deflate stream ends or end of file */
+    do {
+
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)inflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        if (strm.avail_in == 0)
+            break;
+        strm.next_in = in;
+
+        /* run inflate() on input until output buffer not full */
+        do {
+
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+
+            ret = inflate(&strm, Z_NO_FLUSH);
+            fail_unless(ret != Z_STREAM_ERROR, "Zlib inflate state clobbered");  /* state not clobbered */
+            switch (ret) {
+            case Z_NEED_DICT:
+                ret = Z_DATA_ERROR;     /* and fall through */
+            case Z_DATA_ERROR:
+            case Z_MEM_ERROR:
+                (void)inflateEnd(&strm);
+                return ret;
+            }
+
+
+
+            have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)inflateEnd(&strm);
+                return Z_ERRNO;
+            }
+
+
+        } while (strm.avail_out == 0);
+
+        /* done when inflate() says it's done */
+    } while (ret != Z_STREAM_END);
+
+
+    /* clean up and return */
+    (void)inflateEnd(&strm);
+    return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+}
+
 START_TEST (test_zw_create_buffered)
 {
   int len, len2, acc=0;
-  char buf[512], buf2[512];
+  uint8_t buf[512], buf2[512];
   FILE *blob, *f;
   OmlOutStream *os, *fs;
   OmlZlibOutStream *zs;
 
-  /* Remove stray file */
+  /* Remove stray files */
   unlink(ZFN);
+  unlink(ZFN ".blob");
 
   fs = file_stream_new(ZFN);
   os = zlib_stream_new(fs);
@@ -140,24 +283,40 @@ START_TEST (test_zw_create_buffered)
   blob = fopen("blob", "r");
   fail_if(blob == NULL, "Failure opening %s", "blob");
 
+  /*
   while(!feof(blob)) {
     if((len=fread(buf, 1, sizeof(buf), blob))>0) {
       fail_unless(os->write(os, (uint8_t*)buf, len, NULL, 0));
     }
   }
+  os->close(os);
+  */
+
+  /* Dummy compression to make sure the decompression step works */
+  f=fopen(ZFN,"w");
+  fail_unless(def(blob, f, Z_DEFAULT_COMPRESSION) == Z_OK, "Error deflating");
+  fclose(f);
 
   fclose(blob);
-  os->close(os);
+
+  /* Decompress data */
+  f = fopen(ZFN, "r");
+  blob = fopen(ZFN ".blob", "w");
+
+  fail_unless(inf(f,blob)==Z_OK, "Error inflating " ZFN);
+
+  fclose(f);
+  fclose(blob);
 
   /* Check data in the file */
   blob = fopen("blob", "r");
-  f = fopen(ZFN, "r");
+  f = fopen(ZFN ".blob", "r");
   while(!feof(blob) && !feof(f)) {
     if((len=(int)fread(buf, 1, sizeof(buf), blob))>0) {
       fail_unless((len2=fread(buf2, 1, len, f))==len,
-          "Read %d bytes from " ZFN ", expected %d", len2, len);
+          "Read %d bytes from " ZFN ".blob, expected %d", len2, len);
       fail_if(memcmp(buf, buf2, len) != 0,
-          "Contents of blob and " ZFN " differ around offset %d", acc);
+          "Contents of blob and " ZFN ".blob differ around offset %d", acc);
       acc += len;
     }
   }
@@ -165,7 +324,7 @@ START_TEST (test_zw_create_buffered)
   fread(buf,1,1,blob);
   fread(buf2,1,1,f);
   fail_unless (feof(blob) && feof(f),
-      "One of the files is not finished (blob: %d, " ZFN ": %d) after offset %d",
+      "One of the files is not finished (blob: %d, " ZFN ".blob: %d) after offset %d",
       feof(blob), feof(f), acc);
   fclose(f);
   fclose(blob);
