50fdc5ff1e4d483d5850f0cd482eb9bf7fb121d4
Olivier Mehani <olivier.mehani@nicta.com.au>
2014-10-29 13:33:52 +1100

    Split out create_out_stream to a helper using URI components
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- lib/client/oml_out_stream.c
+++ lib/client/oml_out_stream.c
@@ -19,23 +19,72 @@
 #include "oml2/oml_out_stream.h"
 #include "oml_utils.h"
 
+/** Create an OmlOutStream from the components of a parsed URI
+ *
+ * Scheme, and either host/port or path are mandatory.
+ *
+ * \param scheme string containing the selected scheme
+ * \param host string containing the host
+ * \param port string containing the port
+ * \param path string containing the path
+ * \return a pointer to the newly allocated OmlOutStream, or NULL on error
+ *
+ * \see create_out_stream
+ */
+static OmlOutStream*
+create_out_stream_from_components(const char *scheme, const char *hostname, const char *port, const char *filepath)
+{
+  OmlOutStream *os = NULL;
+
+  assert(scheme);
+  assert((hostname && port) || filepath);
+
+  OmlURIType uri_type;
+  uri_type = oml_uri_type(scheme);
+
+  switch (uri_type) {
+  case OML_URI_FILE:
+  case OML_URI_FILE_FLUSH:
+    os = file_stream_new(filepath);
+    if(OML_URI_FILE_FLUSH == uri_type) {
+      file_stream_set_buffered(os, 0);
+    }
+    break;
+
+  case OML_URI_TCP:
+    os = net_stream_new(scheme, hostname, port);
+    break;
+
+  case OML_URI_UDP:
+  case OML_URI_UNKNOWN:
+  default:
+    logwarn ("URI scheme %s is not supported\n", scheme);
+    break;
+  }
+
+  if (os == NULL) {
+    logerror ("Failed to create stream for URI %s:%s[%s]%s%s%s\n",
+        scheme, hostname?"//":"", hostname, port?":":"", port, filepath);
+    return NULL;
+  }
+
+  return os;
+}
+
 /** Create an OmlOutStream for the specified URI */
 OmlOutStream*
 create_out_stream(const char *uri)
 {
-  const char *scheme= NULL;
+  const char *scheme = NULL;
   const char *hostname = NULL;
   const char *port = NULL;
   const char *filepath = NULL;
-  OmlOutStream* os = NULL;
-  OmlURIType uri_type;
+  OmlOutStream *os = NULL;
 
   if (uri == NULL || strlen(uri) < 1) {
     logerror ("Missing or invalid collection URI definition (e.g., --oml-collect)\n");
     return NULL;
   }
-
-  uri_type = oml_uri_type(uri);
 
   if (parse_uri (uri, &scheme, &hostname, &port, &filepath) == -1) {
     logerror ("Error parsing collection URI '%s'; failed to create stream for this destination\n",
@@ -47,18 +96,7 @@ create_out_stream(const char *uri)
     return NULL;
   }
 
-  if (oml_uri_is_file(uri_type)) {
-    os = file_stream_new(filepath);
-    if(OML_URI_FILE_FLUSH == uri_type) {
-      file_stream_set_buffered(os, 0);
-    }
-  } else {
-    os = net_stream_new(scheme, hostname, port);
-  }
-  if (os == NULL) {
-    logerror ("Failed to create stream for URI %s\n", uri);
-    return NULL;
-  }
+  os = create_out_stream_from_components(scheme, hostname, port, filepath);
 
   oml_free ((void*)scheme);
   oml_free ((void*)hostname);
